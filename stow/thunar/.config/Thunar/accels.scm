9a08f84821c2c0fba02a9f9b6e2498be73ca399d
skc <skc@example.com>
2026-09-23 08:00:07 +0530

    auto: 2026-09-23 08:00 | system backup

--- stow/thunar/.config/Thunar/accels.scm
+++ stow/thunar/.config/Thunar/accels.scm
@@ -58,7 +58,6 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-location-selector-entry" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/paste" "<Primary>v")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-in-alt1" "<Primary>KP_Add")
-; (gtk_accel_path "<Actions>/ThunarBookmarks/2f8cad44436659bd115df461246c7a08" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-menubar" "<Primary>m")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/back" "<Alt>Left")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-desktop" "")
